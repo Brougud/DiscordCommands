8d85639f4feacf2e73a2bba4cbcd2d11c53b01b7
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-11 10:59:33 -0400

    Create BotTemplate.js

---
+++ BotTemplate.js
@@ -0,0 +1,6 @@
+const discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-' //this is were you put your prefix so for ex. this would be - so i could do -command and it out put what command is
+
