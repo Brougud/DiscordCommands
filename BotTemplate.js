462342a772ed7caeaa9666568e98fa3a4661b0dc
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-11 11:21:57 -0400

    Update BotTemplate.js

--- BotTemplate.js
+++ BotTemplate.js
@@ -2,5 +2,20 @@ const discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = '-' //this is were you put your prefix so for ex. this would be - so i could do -command and it out put what command is
+const prefix = '\' //this is were you put your prefix so for ex. this would be - so i could do -command and it out put what command is
 
+client.once('ready', () => {
+  console.log('<botname> is loaded'); //this is your loadup message
+});
+
+client.on('message', message => {
+  if(!message.content.startsWith(prefix) || message.author.bot) return; 
+  
+  const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowercase();
+  
+  if(command === 'links') { //ONLY DO THIS IF YOU WANT A SMALL AMOUNT OF COMMADS VERY VERY BAD WAY!!
+    message.channel.send('links here');
+}
+
+client.login('x'); //this is were you put your discords bot token! If you do not know how to do this I will put a tutorial in the readme.md!
