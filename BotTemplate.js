81470872c0d87e3b7939f956620c416443fa8db6
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-11 11:23:50 -0400

    Old prefix kinda broke it

--- BotTemplate.js
+++ BotTemplate.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = '\' //this is were you put your prefix so for ex. this would be - so i could do -command and it out put what command is
+const prefix = "."//this is were you put your prefix so for ex. this would be - so i could do -command and it out put what command is
 
 client.once('ready', () => {
   console.log('<botname> is loaded'); //this is your loadup message
