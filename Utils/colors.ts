8c5e6004f8f093da4984a1020539177e9aec667a
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-12 11:39:53 -0400

    Update colors.ts

--- Utils/colors.ts
+++ Utils/colors.ts
@@ -1,1 +1,13 @@
-const Sucess = '123455'
+export const Sucess = '123455';
+export const Error = '-1242345';
+export const Description = '-12428612';
+
+export const BRIGHT_RED = '-36000000000000000000';
+export const SALMON = '-111235';
+export const BRIGHT_GREEN = '-360000000000';
+export const GREEN = '-36000000000';
+export const GOLD = '-36000000';
+export const LIGHT_PINK = '-360000';
+export const WHITE = '-3';
+export const BLACK = '1';
+export const LIGHT_BLUE = '112356';
