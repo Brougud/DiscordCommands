7481da472ef7cfb3d74b3993aea028bd98e3ecfa
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-18 14:49:30 -0400

    Alternate way

--- Utils/GetUser.js
+++ Utils/GetUser.js
@@ -17,3 +17,32 @@ exports.getUserFromMention = (mention, client) => {
     }
          
   }
+
+//another way is
+
+module.exports = {
+    getMember: function(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.get(toFind);
+        
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+            
+        if (!target) 
+            target = message.member;
+            
+        return target;
+    },
+
+    formatDate: function(date) {
+        return new Intl.DateTimeFormat('en-US').format(date)
+    }
+}
