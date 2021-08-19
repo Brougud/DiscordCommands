df3fbeca33fb3c55138a5fc19107974a61f10ba5
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-19 10:42:27 -0400

    Create userinfo.js

---
+++ Commands/userinfo.js
@@ -0,0 +1,52 @@
+/* What I want
+  - Username
+  - ID
+  - Avatar
+  - Highest role in server
+  - Time in server
+  - Account Age
+  - Playing
+*/
+
+const { getUserFromMention } = require(../utils/GetUser)
+
+modules.export = {
+  name: 'userinfo',
+  aliases: ["who", "user", "info", "whois"],
+  description: 'get the info of a player',
+  usage: "[ username | ID | mention ]"
+  
+  run: (client, message, args) => {
+        const member = getMember(message, args.join(" "));
+
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
+
+        // User variables
+        const created = formatDate(member.user.createdAt);
+
+        const embed = new RichEmbed()
+            .setFooter(member.displayName, member.user.displayAvatarURL)
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+
+            .addField('Member information:', stripIndents`**> Display name:** ${member.displayName}
+            **> Joined at:** ${joined}
+            **> Roles:** ${roles}`, true)
+
+            .addField('User information:', stripIndents`**> ID:** ${member.user.id}
+            **> Username**: ${member.user.username}
+            **> Tag**: ${member.user.tag}
+            **> Created at**: ${created}`, true)
+            
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('Currently playing', stripIndents`**> Name:** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+    }
+}
